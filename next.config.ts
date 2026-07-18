b6c6a22fba617527b1928817c1777702649e1a91
gbcompanyltda <gbcompanyltda@gmail.com>
2026-07-18 19:28:21 -0300

    chore: adicionando workflow do github actions

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/gbcompany-landingpage',
   images: {
     unoptimized: true, // Necessário para o Next.js exportar imagens no GitHub Pages
   },
